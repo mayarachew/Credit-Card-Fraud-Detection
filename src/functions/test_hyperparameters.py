feebc240434fb8029cfbebbcee27933d65b7a996
Mayara Chew Marinho <mayarachew.21@gmail.com>
2022-04-04 21:47:33 -0300

    Add src folder

--- src/functions/test_hyperparameters.py
+++ src/functions/test_hyperparameters.py
@@ -4,31 +4,38 @@ from sklearn.naive_bayes import MultinomialNB # type: ignore
 from sklearn.ensemble import RandomForestClassifier # type: ignore
 from sklearn.svm import SVC # type: ignore
 from sklearn.model_selection import RandomizedSearchCV # type: ignore
-
+from sklearn.neural_network import MLPClassifier # type: ignore
 
 def test_hyperparameters(classifier, X_train, y_train):
     parameters = {}
     classification = None
 
     if classifier == 'Random Forest':
-        parameters = {'n_estimators': [100, 300, 500], "max_depth": [
-            3, 5, 7, None], 'criterion': ['gini', 'entropy'], 'max_features': ['auto', 'sqrt', 'log2']}
+        parameters = {'n_estimators': [100, 300, 500], "max_depth": [3, 5, 7, None], 
+                      'criterion': ['gini', 'entropy'], 'max_features': ['auto', 'sqrt', 'log2']}
         classification = RandomForestClassifier(random_state=0)
+
     elif classifier == 'Naive Bayes':
         parameters = {'alpha': [1, 0.1, 0.01, 0.001, 0.0001]}
         classification = MultinomialNB()
+
     elif classifier == 'Support Vector Machine':
-        parameters = {'kernel': ['rbf', 'poly',
-                                 'sigmoid', 'linear'], "C": [1, 10, 100, 1000]}
+        parameters = {'kernel': ['rbf', 'poly', 'sigmoid', 'linear'], "C": [1, 10, 100, 1000]}
         classification = SVC(random_state=0)
+
+    elif classifier == 'Multilayer Perceptron':
+        parameters = {'hidden_layer_sizes': [(100,), (300,), (500,)], 
+                      'solver': ['sgd', 'adam'],
+                      'alpha': [1, 0.1, 0.01, 0.001, 0.0001],
+                      'learning_rate': ['constant','adaptive']}
+        classification = MLPClassifier(random_state=0)
 
     score = 'f1'
 
     print("# Defining hyperparameters based on %s" % score)
     print()
 
-    clf = RandomizedSearchCV(classification, parameters,
-                             scoring="%s_macro" % score, cv=5, random_state=0)
+    clf = RandomizedSearchCV(classification, parameters, scoring="%s_macro" % score, cv=3)
     clf.fit(X_train, y_train)
 
     print("Best hyperparameters:")
